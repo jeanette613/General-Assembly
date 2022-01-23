6de2f229cbf523a3eadbe8b0eb8ac1c21ff471bc
Melina C <melange@Melinas-Air.hsd1.ma.comcast.net>
2022-01-22 22:10:31 -0500

    preliminary pseudocode for Billiards Game

---
+++ Billiards Project/script.js
@@ -0,0 +1,14 @@
+/*Game will have 3 dynamis spaces
+  First space will be game score board accouncing round winners, turns, scores etc.
+  
+  Second section will be divided cue ball in the forefront
+  ball ineeds to be a div with sections impacting the probability player's shots
+  if ball cant be made interative then multiple buttons should be set aside for the reagions of the cue ball
+  
+  Third there will be a center area that will rotate between the following as the game progresses
+    1) a set of ball set ups/puzzles with each numbered ball and each subsequent ball (this assumes that balls are only made in order)
+      the pocket that the ball should be made into will be predetermined
+    2) a set of videos of each ball being entered into their respective pockets
+    3) a series of videos of each shot being missed
+    4) a series of each cue scratching
+    */
